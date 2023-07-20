7f23b42968e3fb64ae92d3baab11c94f20d3a0fb
ferlarenas4 <fernanda.larenas@ug.uchile.cl>
2023-07-20 19:31:36 -0400

    Vitrina de productos

---
+++ Proyecto Bell/js.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export const Label = () => {
+  return (
+    <div className="label">
+      <div className="div-wrapper">
+        <div className="text-wrapper">Registrarme</div>
+      </div>
+    </div>
+  );
+};
